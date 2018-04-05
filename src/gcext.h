ffc96bef43b3f3e413bc8326f8f706f3c4637dbd
Reimer Behrends <behrends@gmail.com>
2018-04-05 13:10:18 +0200

    GC extension API implementation

--- src/gcext.h
+++ src/gcext.h
@@ -12,14 +12,14 @@ JL_DLLEXPORT extern void (*jl_task_root_scanner_hook)(jl_task_t *task,
 
 
 // Invoke these functions to allocate `bigval_t` instances.
-JL_DLLEXPORT extern void (*jl_nonpool_alloc_hook)(size_t size);
+JL_DLLEXPORT extern void *(*jl_nonpool_alloc_hook)(size_t size);
 JL_DLLEXPORT extern void (*jl_nonpool_free_hook)(void *p);
 
 // Types for mark and sweep functions.
 // We make the cache and sp parameters opaque so that the internals
 // do not get exposed.
-typedef void (*jl_markfunc_t*)(void *cache, void *sp, void *obj);
-typedef void (*jl_sweepfunc_t*)(void *obj);
+typedef void (*jl_markfunc_t)(void *cache, void *sp, void *obj);
+typedef void (*jl_sweepfunc_t)(void *obj);
 
 // Function to create a new foreign type with custom
 // mark and sweep functions.
@@ -33,7 +33,7 @@ JL_DLLEXPORT jl_datatype_t *jl_new_foreign_type(
 
 // Returns the base address of a memory block, assuming it
 // is stored in a julia memory pool. Return NULL otherwise.
-JL_DLLEXPORT jl_value_t jl_pool_base_ptr(void *p);
+JL_DLLEXPORT jl_value_t *jl_pool_base_ptr(void *p);
 
 // Field layout descriptor for custom types that do
 // not fit Julia layout conventions. This is associated with
