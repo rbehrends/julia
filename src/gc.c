262ee64ff0a73294ec453e85606262525b112c1b
Reimer Behrends <behrends@gmail.com>
2018-05-22 18:41:43 +0200

    Introduce `jl_is_internal_obj_alloc()` for conservative scanning.

--- src/gc.c
+++ src/gc.c
@@ -3153,28 +3153,47 @@ STATIC_INLINE int is_valid_pool_obj(jl_taggedvalue_t *val)
 }
 
 
+JL_DLLEXPORT int jl_is_internal_obj_alloc(jl_value_t *p)
+{
+    jl_gc_pagemeta_t* meta = page_metadata(p);
+    if (meta && meta->ages) {
+        char* page = gc_page_data(p);
+        // offset within page.
+        size_t off = (char*)p - page;
+        if (off < GC_PAGE_OFFSET)
+            return 0;
+        // offset within object
+        size_t off2 = (off - GC_PAGE_OFFSET);
+        size_t osize = meta->osize;
+        // if (osize == 0) return NULL;
+        off2 %= osize;
+	if (off2 != sizeof(jl_taggedvalue_t))
+	    return 0;
+        if (off - off2 + osize > GC_PAGE_SZ)
+            return 0;
+        return 1;
+    }
+    return 0;
+}
+
 JL_DLLEXPORT jl_value_t * jl_pool_base_ptr(void *p)
 {
-    /* `p` can point just past the end of the object due
-     * to compiler optimizations. We decrement it by one
-     * byte; this means that the pointer will either lie
-     * within the tag or within the data. */
     p = (char *) p - 1;
     jl_gc_pagemeta_t *meta = page_metadata(p);
     if (meta && meta->ages) {
         char *page = gc_page_data(p);
-       // offset within page.
-       size_t off = (char *) p - page;
-       if (off < GC_PAGE_OFFSET) return NULL;
-       // offset within object
-       size_t off2 = (off - GC_PAGE_OFFSET);
-       size_t osize = meta->osize;
-       // if (osize == 0) return NULL;
-       off2 %= osize;
-       if (off - off2 + osize > GC_PAGE_SZ) return NULL;
-       jl_taggedvalue_t *val = (jl_taggedvalue_t *)((char *)p - off2);
-       if (!is_valid_pool_obj(val)) return NULL;
-       return jl_valueof(val);
+        // offset within page.
+        size_t off = (char *) p - page;
+        if (off < GC_PAGE_OFFSET) return NULL;
+        // offset within object
+        size_t off2 = (off - GC_PAGE_OFFSET);
+        size_t osize = meta->osize;
+        // if (osize == 0) return NULL;
+        off2 %= osize;
+        if (off - off2 + osize > GC_PAGE_SZ) return NULL;
+        jl_taggedvalue_t *val = (jl_taggedvalue_t *)((char *)p - off2);
+	if (!is_valid_pool_obj(val)) return NULL;
+	return jl_valueof(val);
     }
     return NULL;
 }
