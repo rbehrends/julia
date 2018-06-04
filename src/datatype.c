30b572cf0a39b8b82fce0ea18a2bf993b6a0b319
Max Horn <max@quendi.de>
2018-06-04 15:15:52 +0200

    Tabs to spaces, code formatting

--- src/datatype.c
+++ src/datatype.c
@@ -485,30 +485,30 @@ JL_DLLEXPORT jl_datatype_t *jl_new_primitivetype(jl_value_t *name, jl_module_t *
 
 JL_DLLEXPORT jl_datatype_t * jl_new_foreign_type(jl_sym_t *name,
                                                  jl_module_t *module,
-						 jl_datatype_t *super,
-						 jl_markfunc_t markfunc,
-						 jl_finalizefunc_t finalizefunc,
-						 int haspointers,
-						 int large)
+                                                 jl_datatype_t *super,
+                                                 jl_markfunc_t markfunc,
+                                                 jl_finalizefunc_t finalizefunc,
+                                                 int haspointers,
+                                                 int large)
 {
-  jl_datatype_t *bt = jl_new_datatype(name, module, super,
-    jl_emptysvec, jl_emptysvec, jl_emptysvec, 0, 1, 0);
-  bt->size = large ? GC_MAX_SZCLASS+1 : 0;
-  jl_datatype_layout_t *layout =
-    jl_gc_perm_alloc(sizeof(jl_datatype_layout_t) + sizeof(jl_fielddescdyn_t),
-      0, 4, 0);
-  layout->nfields = 0;
-  layout->alignment = sizeof(void *);
-  layout->haspadding = 1;
-  layout->npointers = haspointers;
-  layout->fielddesc_type = 3;
-  jl_fielddescdyn_t * desc =
-    (jl_fielddescdyn_t *) ((char *)layout + sizeof(*layout));
-  desc->markfunc = markfunc;
-  desc->finalizefunc = finalizefunc;
-  bt->layout = layout;
-  bt->instance = NULL;
-  return bt;
+    jl_datatype_t *bt = jl_new_datatype(name, module, super,
+      jl_emptysvec, jl_emptysvec, jl_emptysvec, 0, 1, 0);
+    bt->size = large ? GC_MAX_SZCLASS+1 : 0;
+    jl_datatype_layout_t *layout =
+      jl_gc_perm_alloc(sizeof(jl_datatype_layout_t) + sizeof(jl_fielddescdyn_t),
+        0, 4, 0);
+    layout->nfields = 0;
+    layout->alignment = sizeof(void *);
+    layout->haspadding = 1;
+    layout->npointers = haspointers;
+    layout->fielddesc_type = 3;
+    jl_fielddescdyn_t * desc =
+      (jl_fielddescdyn_t *) ((char *)layout + sizeof(*layout));
+    desc->markfunc = markfunc;
+    desc->finalizefunc = finalizefunc;
+    bt->layout = layout;
+    bt->instance = NULL;
+    return bt;
 }
 
 // bits constructors ----------------------------------------------------------
